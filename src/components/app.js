6fe6a107962b7a048b6680ff7dca94c68caaf5b3
flowstyleliving <mys628@gmail.com>
2016-04-16 06:00:06 -0700

    tutorial finished

--- src/components/app.js
+++ src/components/app.js
@@ -8,8 +8,8 @@ const todos = [
     isComplete: false
   },
   {
-    task: 'go to sleep again',
-    isComplete: false
+    task: 'go to sleep',
+    isComplete: true
   }
 ];
 
@@ -27,10 +27,12 @@ export default class App extends React.Component {
     return (
       <div>
         <h1>React ToDos App</h1>
-        <CreateTodo createTask={this.createTask.bind(this)}/>
+        <CreateTodo todos={this.state.todos} createTask={this.createTask.bind(this)}/>
         <TodosList
           todos={this.state.todos}
           toggleTask={this.toggleTask.bind(this)}
+          saveTask={this.saveTask.bind(this)}
+          deleteTask={this.deleteTask.bind(this)}
         />
       </div>
     );
@@ -49,5 +51,15 @@ export default class App extends React.Component {
     });
     this.setState({todos: this.state.todos});
   }
-  
+
+  saveTask(oldTask, newTask) {
+    const foundTodo = _.find(this.state.todos, todo => todo.task === oldTask);
+    foundTodo.task = newTask;
+    this.setState({todos: this.state.todos});
+  }
+
+  deleteTask(taskToDelete) {
+    _.remove(this.state.todos, todo => todo.task === taskToDelete);
+    this.setState({todos: this.state.todos});
+  }
 }
